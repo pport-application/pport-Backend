6ab2d5573fcfd3ea36afd7b2a8af6153c72e6d8d
akmami <akmuhammet99@gmail.com>
2022-10-22 14:12:27 +0300

    sign in endpoint implemented

--- api/models.py
+++ api/models.py
@@ -1,16 +1,16 @@
-from django.db import models
 from django.contrib.auth.models import User
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
+
 
 # Create your models here.
 class UserSerializers(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'password')
-        extra_kwargs = {'password': {'write_only': True, 'required': True }}
+        extra_kwargs = {'password': {'write_only': True, 'required': True}}
 
-    def create(self, validated_data): # using predefined create_user function for password encryption
+    def create(self, validated_data):  # using predefined create_user function for password encryption
         user = User.objects.create_user(**validated_data)
-        Token.objects.create(user=user) # create token immediately (can be returned)
+        Token.objects.create(user=user)  # create token immediately (can be returned)
         return user
